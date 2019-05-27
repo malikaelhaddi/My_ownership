edfd1d1915fd30c628b0f8373e2321e568a48324
shivamspj <31476741+shivamspj@users.noreply.github.com>
2019-05-27 12:51:29 +0530

    Update eq_solver.py

--- eq_solver.py
+++ eq_solver.py
@@ -23,7 +23,7 @@ beta2 =[]
 beta3 =[]
 
 
-xl_workbook = xlrd.open_workbook("C:/Users/kumar.shivam/Desktop/25-05-2018/dataset.xlsx")
+xl_workbook = xlrd.open_workbook("/Desktop/25-05-2018/dataset.xlsx")
 sheet = xl_workbook.sheet_by_index(1)
 
 
@@ -77,113 +77,7 @@ ax.set_zlabel('beta3')
 
 plt.show()
 
-fig.savefig('myimage1.svg', format='svg', dpi=1200)
-#result = (equations((a,b,c)))
-#print(result)
 
-#
-#wmax = 40.93423843
-#n = 4
-#dT = 0.0034
-#
-##a = result[0]
-##b = result[1]
-##c = result[2]
-#
-#dTx = (1-(1-1/(1+math.exp(a+b*(wmax))))**(n**c))
-#
-#print(dTx)
-##
-#a = 0.1
-#b = 0.5
-#c = -44
-#wmax = 41
-#n = 2
-#dT = 3071.516798
-#
-#
-#za =1-(1-(1/(1+np.exp((a+b*(wmax)))**(n**c)))) - dT
-#
-#dT = (1-(za))
-#print(dT)
-#print(za)
-
-#from scipy.optimize import fsolve
-#import numpy as np
-#import xlrd
-#
-#xl_workbook = xlrd.open_workbook("C:/Users/kumar.shivam/Desktop/25-05-2018/dataset.xlsx")
-#sheet = xl_workbook.sheet_by_index(1)
-#
-##print(sheet.cell_value(0,10),'va')
-#
-#
-#d = []
-#w = [] 
-#
-#for i in range(1,161):
-#    d.append([float(sheet.cell_value(i,10)),float(sheet.cell_value(i+1,10))])
-#    w.append([float(sheet.cell_value(i,11)),float(sheet.cell_value(i+1,11))])
-#
-#w1 = 54
-#w2 = 55
-#
-#d1 = 0.000412 
-#d2 = 0.000467 
-#  
-#
-#def equations(p):
-#    a, b = p 
-#    return((1/(1+2.718281**(a + b*(w1))) - d1),(1/(1+2.718281**(a + b*(w2))) - d2))
-##    try:
-#
-##    except:
-##        return(float('inf'))
-#
-#a,b = fsolve(equations, (0.01, 0.01), maxfev=1000)
-#
-#print(a,b)
-#print(equations((a,b)))
-#
-#
-#ax = 14.5635699174
-#bx = -0.125360970709
-#w1x = 54
-##d1x = 0.000059 
-#d1x = (1/(1+np.exp(ax + bx*(w1x))))
-#print(
-#
-#n = np.log(40)
-#dT = np.log(np.log(3071.516798))
-#
-#n1 = np.log(40)
-#dT1 = np.log(np.log(4213.211399))
-#
-#
-#
-#
-#def equations(p):
-#    a, b = p 
-#    return(dT - b*(a+n)),(dT - b*(a+n))
-#
-##
-##def equations(p):
-##    a, b = p 
-##    return(np.log(dT) - a*n**b),(np.log(dT1) - a*n1**b)
-#
-#
-#a,b = fsolve(equations, (0.001, 0.001), maxfev=1000)
-#
-#print(a,b)
-#print(equations((a,b)))
-#
-
-
-#a=96.7398544963 
-#b=0.0207428225764
-#x = (dT - b*(a+n))
-#print(x)
-#d = 1/(1+math.exp(βo +β1*(wmax)))
 
 
 
